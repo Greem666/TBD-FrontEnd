e1e5388f151b4f42bd98562506c00e614c8aee0d
Greem666 <29668023+Greem666@users.noreply.github.com>
2020-11-11 23:31:58 +1100

    First commit

---
+++ src/test/java/com/greem/project/ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greem.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class git ProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
